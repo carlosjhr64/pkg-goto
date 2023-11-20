c673bf900766a3577f835288be4c06d861f4973e
CarlosJHR64 <carlosjhr64@gmail.com>
2023-11-20 13:45:03 -0800

    Build bump. Mostly comments for readability.

--- functions/goto.fish
+++ functions/goto.fish
@@ -5,15 +5,20 @@ function pkg-goto_trace
 end
 
 function pkg-goto_env
-  set -l d ''
-  set -l base $argv[1]
-  set -l key $argv[2]
+  set --local d ''
+  set --local base $argv[1]
+  set --local key $argv[2]
+  # Check if key might be an environment variable.
   if string match --quiet --regex '^\w+$' "$key"
+    # If so, then check if it is set.
     for path in $$key
+      # If set, then check if base/path is a directory.
       if test -d "$base$path"
         set d "$base$path"
         break
       else
+        # Else if base is HOME, then maybe path is giving the full path and
+        # base is not needed.
         if test "$base" = "$HOME/"
           if test -d "$path"
             set d "$path"
@@ -27,13 +32,14 @@ function pkg-goto_env
 end
 
 function pkg-goto_find
-  set -l d ''
-  set -l n 1
-  while test $n -le 4
+  set --local d ''
+  set --local n 1
+  while test $n -le 4 # Limit mindepth to 4.
     set d (find -L $argv[1] -mindepth $n -maxdepth $n -type d -name "$argv[2]")
     if test (count $d) -gt 1
-      set pkg_goto_trace '1'
+      # If more than one result, then find the most recently modified.
       set d (ls -td $d | egrep -v '/\.' | head -1)
+      set pkg_goto_trace '1' # Due to ambiguity, report the path selected.
     end
     if test -n "$d"
       break
@@ -47,10 +53,10 @@ function pkg-goto_find
 end
 
 function pkg-goto_finds
-  set -l d ~/
-  for a in $argv
-    set d (pkg-goto_find $d $a)
-    if test -z "$d"
+  set --local d ~/
+  for a in $argv # For each argument, find a path to it...
+    set d (pkg-goto_find $d $a) # ...appending to the previous path found.
+    if test -z "$d" # Break out if no path found.
       break
     end
   end
@@ -58,11 +64,11 @@ function pkg-goto_finds
 end
 
 function pkg-goto_found
-  set -l d (pkg-goto_finds $argv)
-  if test -n "$d"
+  set --local d (pkg-goto_finds $argv) # Set to found path.
+  if test -n "$d" # If path found, then cd to it.
     if builtin cd $d[1]
       pkg-goto_trace "#"(pwd)
-      if functions -q fish_greeting
+      if functions --query fish_greeting
         fish_greeting
       end
       return 0
@@ -79,11 +85,11 @@ function pkg-goto_found
 end
 
 function goto
-  set -g pkg_goto_trace ''
-  set -l error ''
+  set --global pkg_goto_trace '' # Set to '1' to trace.
+  set --local error '' # Set to '1' to indicate error.
   if argparse --name=goto 'h/help' 'v/version' 't/trace' -- $argv
     if test -n "$_flag_version"
-      echo '1.2.0'
+      echo '1.2.1'
     else if test -n "$_flag_help"
       echo 'Usage: goto [-t --trace] <basename>...'
     else
@@ -97,7 +103,7 @@ function goto
   else
     set error '1'
   end
-  set -e pkg_goto_trace
+  set --erase pkg_goto_trace
   if test -n "$error"
     false
   else
